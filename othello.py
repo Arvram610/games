396dbdbcec66b3dbd02b8ce7e61704ee277aa586
Arvram610 <arvid.ramsberg@gmail.com>
2020-01-25 20:42:57 +0100

    added +x-y diagonal added +y and -y

--- othello.py
+++ othello.py
@@ -13,6 +13,7 @@ class Brick:
 
 class Playfield:
     def __init__(self, height, length):
+        self.game_on = True
         self.ask = input("Input player one name: ")
         self.p1_name = self.ask
         self.ask = input("Input player two name: ")
@@ -68,8 +69,9 @@ class Playfield:
             x = False
             y_positive = 8 - position_y
             x_positive = 8 - position_x
-            x_negative = position_x - 1
-            y_negative = position_y - 1
+            x_negative = position_x -1
+            y_negative = position_y -1
+
             if self.bricks[y_negative][x_negative].side == not_side:  # diagonal -x -y
                 if y_negative > x_positive:
                     y = x_negative + 1
@@ -112,19 +114,17 @@ class Playfield:
                         preliminary_list = []
                         break
 
-            if self.bricks[position_y][x_negative].side == not_side:  # negative y
-                for i in range(x_negative):
+            if self.bricks[y_negative][position_x].side == not_side:  # negative y
 
+                for i in range(position_y):
+                    if self.bricks[y_negative-i][position_x].side == not_side:
+                        preliminary_list.append(self.bricks[position_y - (i + 1)][position_x])
 
-                    if self.bricks[position_y][x_negative - i].side == not_side:
-
-                        preliminary_list.append(self.bricks[position_y][position_x - (i + 1)])
-
-
-                    elif self.bricks[position_y][x_negative - i].side == side and not position_x - i == position_x - 0:
+                    elif self.bricks[y_negative-i][position_x].side == side and not position_y-i == position_y-0:
                         for j in preliminary_list:
                             list.append(j)
                             x = True
+
                         preliminary_list = []
                         break
 
@@ -132,14 +132,77 @@ class Playfield:
                         preliminary_list = []
                         break
 
-            if x == True:  # byt spelares tur
+            if self.bricks[y_negative][position_x+1].side == not_side:  # diagonal +x -y
+                if y_negative+1 > x_positive-1:
+                    y = y_negative
+
+                else:
+                    y = x_positive
+
+                for i in range(y):
+                    if self.bricks[position_y - i-1][position_x + i + 1].side == not_side:
+                        preliminary_list.append(self.bricks[position_y - i-1][position_x + i + 1])
+
+                    elif self.bricks[position_y - i-1][position_x + i + 1].side == side and not position_x + (
+                            i + 1) == position_x + 1 and not position_y + (i + 1) == position_y + 1:
+                        for j in preliminary_list:
+                            list.append(j)
+
+                        preliminary_list = []
+                        x = True
+                        break
+
+                    else:
+                        print("hello3")
+                        preliminary_list = []
+                        break
+
+            if self.bricks[position_y][position_x+1].side == not_side:  # positive x
+
+                for i in range(x_positive):
+                    if self.bricks[position_y][position_x + i+1].side == not_side:
+
+                        preliminary_list.append(self.bricks[position_y][position_x + i+1])
+
+                    elif self.bricks[position_y][position_x + i+1].side == side and not position_x - i == position_x - 0:
+                        for j in preliminary_list:
+                            list.append(j)
+                            x = True
+
+                        preliminary_list = []
+                        break
+
+                    else:
+                        preliminary_list = []
+                        break
+            try:
+                if self.bricks[position_y+1][position_x].side == not_side:  # positive y
+
+                    for i in range(y_positive):
+                        if self.bricks[position_y+1+i][position_x].side == not_side:
+                            preliminary_list.append(self.bricks[position_y + (i + 1)][position_x])
+
+                        elif self.bricks[position_y+1+i][position_x].side == side and not position_y-i == position_y-0:
+                            for j in preliminary_list:
+                                list.append(j)
+                                x = True
+
+                            preliminary_list = []
+                            break
+
+                        else:
+                            preliminary_list = []
+                            break
+            except:
+                pass
+            if x:  # byt spelares tur
                 list.append(self.bricks[position_y][position_x])
                 if self.turn == "p1":
                     self.turn = "p2"
 
-
                 else:
                     self.turn = "p1"
+
             for i in list:
                 i.side = side
                 i.pchangeble = False
@@ -165,9 +228,9 @@ class Playfield:
 
     def play(self):
 
-        game_on = True
-        while game_on == True:
-            try:
+
+        while self.game_on:
+            #try:
                 ask = input(self.turn + " input coordinates (x and y, seperate with \",\"): ")
                 if ask == "DeBuG":
                     ask = input(self.turn + "input coordinates (x and y, seperate with \",\" )debug: ")
@@ -197,10 +260,10 @@ class Playfield:
 
                     if self.bricks[self.position_y][self.position_x].pchangeble == True:
                         self.player_change(self.position_x, self.position_y, self.turn)
-            except:
-                clearscreen()
-                self.print()
-                print("oops, something went wrong...")
+           # except:
+              #  clearscreen()
+              #  self.print()
+             #   print("oops, something went wrong...")
 
 
 x = Playfield(8, 8)
