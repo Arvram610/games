0c5d214e76023c530fd33d52fd582e7762aeb474
arvram610 <arvid.ramsberg@gmail.com>
2020-02-04 14:11:21 +0100

    fixed last push

--- othello.py
+++ othello.py
@@ -3,18 +3,19 @@ import os
 
 
 def clearscreen(numlines=100):
-  """Clear the console.
-numlines is an optional argument used only as a fall-back.
-"""
+    pass
+  #"""Clear the console.
+#numlines is an optional argument used only as a fall-back.
+#"""
 # Thanks to Steven D'Aprano, http://www.velocityreviews.com/forums
 
-  if os.name == "posix":
+ # if os.name == "posix":
     # Unix/Linux/MacOS/BSD/etc
-    os.system('clear')
-  elif os.name in ("nt", "dos", "ce"):
+  #  os.system('clear')
+ # elif os.name in ("nt", "dos", "ce"):
     # DOS/Windows
-    os.system('CLS')
-  else:
+  #  os.system('CLS')
+  #else:
     # Fallback for other operating systems.
     print('\n' * numlines)
 
